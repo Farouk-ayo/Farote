a8ccaa4b772fc276f49c127f9f790d813204f27a
Farouk-ayo <mustaphafarouk41@gmail.com>
2025-04-20 10:52:49 +0100

    fix: `Removed NextAuth authentication setup and related files`

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -2,7 +2,7 @@ import connectToDatabase from "@/lib/mongodb";
 import Note from "@/models/note";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/(auth)/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface Params {
   params: {
